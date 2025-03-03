f927b148defb02c134dce7ddba179e43fbe747b6
Anshul Rai <anshulrai02092003@gmail.com>
2025-03-03 15:57:30 +0530

    [Anshul] Add . UC4 - Added ability to find and edit contact by name

--- address_book_js/src/index.js
+++ address_book_js/src/index.js
@@ -5,15 +5,17 @@ const addressBook = new AddressBook();
 
 try {
     const contact1 = new Contact(
-        "Anand", "Soni", "Bhopal", "India", "In", "100001", "9876543210", "anandsoni9165@gmail.com"
+        "Anshul", "Rai", "Bhopal", "India", "In", "100001", "9876543210", "anshulrai077@gmail.com"
     );
     console.log(addressBook.addContact(contact1)); 
 
     const contact2 = new Contact(
-        "Rakesh", "Soni", "Bhopal", "India","In","1000000","9197979988","rakeshsoni395@gmail.com"
+        "Ram", "Rai", "Bhopal", "India","In","1000000","9197979988","ramrai395@gmail.com"
     )
     console.log(addressBook.addContact(contact2)); 
-    console.log("All Contacts", addressBook.getAllContacts());
+    console.log("All Contacts Before editing", addressBook.getAllContacts());
+    console.log(addressBook.editContact("Rakesh", { address: "Pipariya", city: "New York", phone: "9234567890" }));
+    console.log("All Contacts After editing", addressBook.getAllContacts());
 } catch (error) {
     console.error("Error:", error.message);
 }
