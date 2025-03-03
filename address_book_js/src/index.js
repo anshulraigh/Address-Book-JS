1948bea8d0116947529e0af20be16f9fc6003baf
Anshul Rai <anshulrai02092003@gmail.com>
2025-03-03 16:02:26 +0530

    [Anshul] Add. UC5 - Added ability to find and delete contact by name

--- address_book_js/src/index.js
+++ address_book_js/src/index.js
@@ -14,8 +14,10 @@ try {
     )
     console.log(addressBook.addContact(contact2)); 
     console.log("All Contacts Before editing", addressBook.getAllContacts());
-    console.log(addressBook.editContact("Rakesh", { address: "Pipariya", city: "New York", phone: "9234567890" }));
+    console.log(addressBook.editContact("Ram", { address: "Pipariya", city: "New York", phone: "9234567890" }));
     console.log("All Contacts After editing", addressBook.getAllContacts());
+    console.log(addressBook.deleteContact("Ram"));
+    console.log("All Contacts After Deleting:", addressBook.getAllContacts());
 } catch (error) {
     console.error("Error:", error.message);
 }
